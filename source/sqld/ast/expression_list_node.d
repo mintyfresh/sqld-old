2126f8abdf074a9de02efc0faa6c1c1bd56472d4
Mihail-K <mihail@platterz.ca>
2017-04-24 19:21:28 -0400

    Add helper for building expression list nodes from expression types.

--- source/sqld/ast/expression_list_node.d
+++ source/sqld/ast/expression_list_node.d
@@ -6,6 +6,7 @@ import sqld.ast.visitor;
 
 import std.algorithm;
 import std.array;
+import std.meta;
 
 class ExpressionListNode : ExpressionNode
 {
@@ -46,4 +47,14 @@ immutable(ExpressionNode)[] flattenExpressionList(immutable(ExpressionNode) node
 {
     auto list = cast(immutable(ExpressionListNode)) node;
     return list ? list.nodes : [node];
+}
+
+immutable(ExpressionListNode) expressionList(TList...)(TList args) if(allSatisfy!(isExpressionType, TList))
+{
+    return new immutable ExpressionListNode(mixin("[" ~ [staticMap!(wrapExpression, args)].join(", ") ~ "]"));
+}
+
+private template wrapExpression(alias expression)
+{
+    enum wrapExpression = "expression(" ~ __traits(identifier, expression) ~ ")";
 }
