b8ce67b033395195b0185423ecb26c809a0d6a24
Mihail-K <mihail@platterz.ca>
2017-04-29 16:43:48 -0400

    Add helper for between operations.

--- source/sqld/ast/between_node.d
+++ source/sqld/ast/between_node.d
@@ -14,3 +14,21 @@ immutable class BetweenNode : TernaryNode
         super(first, second, third);
     }
 }
+
+immutable(BetweenNode) between(T1, T2, T3)(T1 first, T2 second, T3 third)
+    if(isExpressionType!(T1) && isExpressionType!(T2) && isExpressionType!(T3))
+{
+    return new immutable BetweenNode(toExpression(first), toExpression(second), toExpression(third));
+}
+
+@system unittest
+{
+    import sqld.ast.table_node : table;
+    import sqld.test.test_visitor : TestVisitor;
+
+    auto v = new TestVisitor;
+    auto n = 10.between(5, 15);
+
+    n.accept(v);
+    assert(v.sql == "10 BETWEEN 5 AND 15");
+}
